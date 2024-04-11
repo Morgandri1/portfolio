52b5aba0e50dd175e359ea4a07330dbb9d990e18
Morgan Metz <mlmi4@k12albemarle.org>
2024-04-11 01:26:08 -0400

    update

--- src/content_option.js
+++ src/content_option.js
@@ -158,22 +158,20 @@ const dataportfolio = [
 ];
 
 const contactConfig = {
-  YOUR_EMAIL: "name@domain.com",
-  YOUR_FONE: "(555)123-4567",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  YOUR_EMAIL: "morgan.metz@eyekon.xyz",
+  YOUR_FONE: "(434)260-9485",
+  description: "Text or email me at any time with any questions.",
   // creat an emailjs.com account
   // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-  YOUR_SERVICE_ID: "service_id",
-  YOUR_TEMPLATE_ID: "template_id",
-  YOUR_USER_ID: "user_id",
+  YOUR_SERVICE_ID: "service_x6fdyom",
+  YOUR_TEMPLATE_ID: "template_7lyjuwu",
+  YOUR_USER_ID: "93j0pl248GyGT_N2o",
 };
 
 const socialprofils = {
-  github: "https://github.com",
-  facebook: "https://facebook.com",
-  linkedin: "https://linkedin.com",
-  twitter: "https://twitter.com",
+  github: "https://github.com/morgandri1",
+  linkedin: "https://linkedin.com/in/morgandri1",
+  twitter: "https://twitter.com/Morgandri1dev",
 };
 export {
   meta,
