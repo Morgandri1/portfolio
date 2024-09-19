e0bc9eca1ff0286338eddb6e75857a286f67ae8a
Morgan Metz <68765538+Morgandri1@users.noreply.github.com>
2024-09-19 12:04:26 -0400

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -2,7 +2,7 @@ const logotext = "";
 const meta = {
   title: "Morgan Metz - Portfolio",
   description:
-    "I'm a full-stack web3-focused engineer based in Charlottesville, VA.",
+    "I'm a full-stack software engineer based in Charlottesville, VA.",
 };
 
 const introdata = {
@@ -13,14 +13,14 @@ const introdata = {
     third: "I love open source",
   },
   description:
-    "I'm an experienced backend web3-focused engineer based in Charlottesville, VA.",
+    "I'm an experienced full-stack software engineer based in Charlottesville, VA.",
   your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
   title: "Introduction",
   aboutme:
-    "I’m a young, but experienced backend/api developer who has spent the last 5 years working on blockchain systems and building web3 technology. As of June 2026, I will have been building production (backend) systems for 5 years, as well as having had contributed to open source projects, 6 years of experience with python, and 4 years of experience with rust. This, combined with my previous experience working on large scale distributed systems and my out-of-the-box thinking, has lead me to become an extremely capable developer even at a young age.",
+    "I’m a young, but experienced backend/api developer who has spent the last 2 years working on blockchain systems and building web3 technology. As of June 2026, I will have been building production (backend) systems for 5 years, as well as having had contributed to open source projects, 6 years of experience with python, and 4 years of experience with rust. This, combined with my previous experience working on large scale distributed systems and my out-of-the-box thinking, has allowed me to become an extremely capable developer even at a young age.",
 };
 
 const skills = [
@@ -34,11 +34,11 @@ const skills = [
   },
   {
     name: "Typescript",
-    value: 80,
+    value: 90,
   },
   {
     name: "React",
-    value: 60,
+    value: 75,
   },
   {
     name: "Rust",
@@ -48,6 +48,10 @@ const skills = [
     name: "Python",
     value: 100,
   },
+  {
+    name: "Golang",
+    value: 45
+  }
 ];
 
 const services = [
@@ -70,7 +74,7 @@ const dataportfolio = [
   {
     img: "/rift.png",
     description: "Mobile Web3 IRL event planner",
-    link: "#",
+    link: "",
   },
   {
     img: "/stakit.jpeg",
@@ -86,7 +90,7 @@ const dataportfolio = [
   {
     img: "/copperhead.jpeg",
     description: "A solana-based development agency later acquired by EYEKON",
-    link: "#",
+    link: "",
     onClick: () => {
       alert("No maintained website for copperhead after being EOLed by EYEKON");
     },
@@ -101,7 +105,7 @@ const dataportfolio = [
     img: "/unison.png",
     description:
       "A multichain wallet operating on 16 different chains. Never saw light of day due to founder (and funding) disapearance. I built an intricate API to allow users to interact with different chains easily.",
-    link: "#",
+    link: "",
     onClick: () => {
       alert(
         "There is no maintained website for unison after funding was rugged.",
@@ -112,7 +116,7 @@ const dataportfolio = [
 
 const contactConfig = {
   YOUR_EMAIL: "morgan.metz@eyekon.xyz",
-  YOUR_FONE: "(434)260-9485",
+  YOUR_FONE: "(434) 260-9485",
   description: "Text or email me at any time with any questions.",
   // creat an emailjs.com account
   // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
